0af342b12214ce7fb55e8c04f9f3cbf30c5ff0ba
samzerox <sam.zerox88@gmail.com>
2018-04-18 13:21:52 -0500

    Peticiones de tecnologia y ventana terminadas

--- routes/tecnologia.js
+++ routes/tecnologia.js
@@ -8,7 +8,7 @@ var Tecnologia = require('../models/tecnologia');
 
 
 //======================================
-// Obtener todos los datos
+// Obtener todos las tecnologias
 //======================================
 app.get('/', (req, res) => {
     Tecnologia.find({})
@@ -29,5 +29,145 @@ app.get('/', (req, res) => {
 
         });
 });
+
+
+//======================================
+// Obtener Tecnologia por Id
+//======================================
+app.get('/:id', (req, res) => {
+    var id = req.params.id;
+    Tecnologia.findById(id)
+        .exec((err, tecnologia) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar tecnologia',
+                    errors: err
+                });
+            }
+            if (!tecnologia) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'la tecnologia con el id ' + id + 'no existe',
+                    errors: { message: 'No existe una tecnologia con ese ID' }
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                tecnologia: tecnologia
+            });
+        });
+});
+
+
+//======================================
+// Actualizar una tecnologia
+//======================================
+app.put('/:id', (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Tecnologia.findById(id, (err, tecnologia) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar tecnologia',
+                errors: err
+            });
+        }
+
+        if (!tecnologia) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'la tecnologia con el id ' + id + ' no existe',
+                errors: { message: 'No existe una tecnologia con ese ID' }
+            });
+        }
+
+        tecnologia.nombre = body.nombre;
+        tecnologia.codigo = body.codigo;
+
+        tecnologia.save((err, tecnologiaGuardado) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar tecnologia',
+                    errors: err
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                tecnologia: tecnologiaGuardado
+            });
+        });
+    });
+});
+
+
+//======================================
+// Crear una nueva tecnologia
+//======================================
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    var tecnologia = new Tecnologia({
+        nombre: body.nombre,
+        codigo: body.codigo,
+
+    })
+
+    tecnologia.save((err, tecnologiaGuardado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al crear la tecnologia',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            tecnologia: tecnologiaGuardado,
+        });
+    });
+});
+
+
+//======================================
+// Eliminar una tecnologia
+//======================================
+app.delete('/:id', (req, res) => {
+    var id = req.params.id;
+
+    Tecnologia.findByIdAndRemove(id, (err, tecnologiaBorrado) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaaje: 'Error al borrar la tecnologia',
+                errors: err
+            });
+        }
+
+        if (!tecnologiaBorrado) {
+            return res.status(404).json({
+                ok: false,
+                mensaje: 'No existe una tecnologia con ese id',
+                errors: { message: 'No existe una tecnologia con ese id' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            tecnologia: tecnologiaBorrado
+        });
+    });
+
+});
+
+
 
 module.exports = app;
--- routes/ventana.js
+++ routes/ventana.js
@@ -30,4 +30,146 @@ app.get('/', (req, res) => {
         });
 });
 
+
+//======================================
+// Obtener Tecnologia por Id
+//======================================
+app.get('/:id', (req, res) => {
+    var id = req.params.id;
+    Ventana.findById(id)
+        .exec((err, ventana) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar ventana',
+                    errors: err
+                });
+            }
+            if (!ventana) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'la ventana con el id ' + id + 'no existe',
+                    errors: { message: 'No existe una ventana con ese ID' }
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                ventana: ventana
+            });
+        });
+});
+
+
+//======================================
+// Actualizar una tecnologia
+//======================================
+app.put('/:id', (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Ventana.findById(id, (err, ventana) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar ventana',
+                errors: err
+            });
+        }
+
+        if (!ventana) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'la ventana con el id ' + id + ' no existe',
+                errors: { message: 'No existe una ventana con ese ID' }
+            });
+        }
+
+        ventana.titulo = body.titulo;
+        ventana.descripcion = body.descripcion;
+
+        ventana.save((err, ventanaGuardado) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar ventana',
+                    errors: err
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                ventana: ventanaGuardado
+            });
+        });
+    });
+});
+
+
+//======================================
+// Crear una nuevo tecnologia
+//======================================
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    var ventana = new Ventana({
+        titulo: body.titulo,
+        descripcion: body.descripcion
+
+    })
+
+    ventana.save((err, ventanaGuardado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al crear el ventana',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            ventana: ventanaGuardado,
+        });
+    });
+});
+
+
+//======================================
+// Eliminar una ventana
+//======================================
+app.delete('/:id', (req, res) => {
+    var id = req.params.id;
+
+    Ventana.findByIdAndRemove(id, (err, ventanaBorrado) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaaje: 'Error al borrar la ventana',
+                errors: err
+            });
+        }
+
+        if (!ventanaBorrado) {
+            return res.status(404).json({
+                ok: false,
+                mensaje: 'No existe una ventana con ese id',
+                errors: { message: 'No existe una ventana con ese id' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            ventana: ventanaBorrado
+        });
+    });
+
+});
+
+
+
+
+
 module.exports = app;
