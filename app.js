c63bb9323563fcb8a3ad59a23d6920332beae482
samzerox <sam.zerox88@gmail.com>
2018-04-23 19:56:19 -0500

    se agrego la variable port

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use('/', appRoutes);
 
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 
 });
